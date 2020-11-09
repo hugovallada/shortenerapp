792c7016aed32a8b95699aea841cba94080a55ca
Hugo Vallada <valladahugo@gmail.com>
2020-11-09 14:24:30 -0300

    Adicionada a funçao de copiar com 1 clique

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import './Form.css';
+
 
 export default () => {
     const [url, setUrl] = useState('');
@@ -13,34 +15,56 @@ export default () => {
     const handleClick = async (event) => {
         event.preventDefault();
         console.log(url);
-        try{
+        try {
             const response = await fetch('http://localhost:3001/encurtar', {
                 method: "POST",
                 body: JSON.stringify({
                     url
                 }),
                 headers: {
-                    "Content-type":"application/json"
+                    "Content-type": "application/json"
                 }
             });
-    
+
             const data = await response.text();
             setShortUrl(data);
-        }catch(error){
+        } catch (error) {
             console.log(error);
         }
-        
+
+    }
+
+
+    // função para copiar com click
+    const handleCopy = (event) => {
+        const el = document.createElement('textarea');
+        el.value = shortUrl;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
+
+    const handleLink = () => {
+        window.location.replace(shortUrl);
     }
 
 
     return (
-        <div>
+        <div class="form-div">
             <form method="POST">
                 <input type="text" name="url" value={url} onInput={handleInputUrl} />
                 <button type="submit" onClick={handleClick}> Encurtar </button>
             </form>
-
-            <h2>{shortUrl}</h2>
+            {shortUrl 
+            ? 
+            <div>
+                <button type="button" onClick={handleCopy}>Copy</button>
+                <p>{shortUrl}</p>
+            </div>
+            :
+            shortUrl
+            }
         </div>
     )
 
